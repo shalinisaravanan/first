f34ec8d035f51c8cae3c21463a76f7e309e7fea0
shalinisaravanan <34339975+shalinisaravanan@users.noreply.github.com>
2018-06-22 05:55:17 -0700

    Update and rename odd.java to character.java

---
+++ character.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class character
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char c=sc.next();
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+{
+System.out.print("vowel");
+}
+else
+{
+System.out.print("consonant");
+}
+}
+}
