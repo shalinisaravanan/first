a0e3aac32f182022953f0f487fd3a52e4832574d
shalinisaravanan <34339975+shalinisaravanan@users.noreply.github.com>
2018-06-22 06:01:27 -0700

    Update character.java

--- character.java
+++ character.java
@@ -5,7 +5,7 @@ class character
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-char c=sc.next();
+char c=sc.next().charAt(0);
 if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
 {
 System.out.print("vowel");
