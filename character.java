4932d5c3e53f9805605c9b776e406877a80ac87d
shalinisaravanan <34339975+shalinisaravanan@users.noreply.github.com>
2018-06-22 06:36:47 -0700

    Update character.java

--- character.java
+++ character.java
@@ -6,13 +6,19 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 char c=sc.next().charAt(0);
+ if(c>='a'&&c<='z')
+ {
 if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
-{
+
 System.out.print("vowel");
-}
+
+else
+
+System.out.print("consonant");
+ }
 else
 {
-System.out.print("consonant");
+  System.out.print("Invalid");
 }
 }
 }
