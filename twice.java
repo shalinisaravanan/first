adda3d506def0417e1da46fad81b5708185f47c1
shalinisaravanan <34339975+shalinisaravanan@users.noreply.github.com>
2018-06-26 08:08:26 +0000

    Create twice.java

---
+++ twice.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+class repeat
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int a[]=new int[n];
+    int flag=0;
+    for(int i=0;i<n;i++)
+    {
+      a[i]=sc.nextInt();
+    }
+    Arrays.sort();
+    for(int i=0;i<a.length-1;i++)
+    {
+    for(int j=i+1;j<a.length;j++)
+    {
+    if(a[i]!=a[j])
+    {
+    System.out.print(a[i]);
+    break;
+    }
+    }
+    }
