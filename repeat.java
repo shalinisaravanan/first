1db14a69e1278a6bd0d896f0f775a1df7c1424a6
shalinisaravanan <34339975+shalinisaravanan@users.noreply.github.com>
2018-06-26 06:44:47 +0000

    Update repeat.java

--- repeat.java
+++ repeat.java
@@ -7,6 +7,7 @@ class repeat
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
     int a[]=new int[n];
+    int flag=0;
     for(int i=0;i<n;i++)
     {
       a[i]=sc.nextInt();
@@ -19,9 +20,14 @@ class repeat
         if(a[i]==a[j])
         {
           System.out.print(a[i]);
+          flag=1;
           break;
         }
       }
+    }
+      if(flag==0)
+    {
+    	System.out.print("Unique");
     }
   }
 }
